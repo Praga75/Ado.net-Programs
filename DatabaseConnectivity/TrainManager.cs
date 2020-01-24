c653abafa11b7134f46fabc4ee8a3f9d5ada716a
Praga75 <42761214+Praga75@users.noreply.github.com>
2020-01-24 16:29:40 +0530

    Add files via upload

--- DatabaseConnectivity/TrainManager.cs
+++ DatabaseConnectivity/TrainManager.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DatabaseConnectivity
 {
-    class TrainManager
+    class TrainManagerUsingDataAdapter
     {
         public void AddTrainInfo(SqlConnection connection)
         {
@@ -21,48 +22,35 @@ namespace DatabaseConnectivity
             Console.WriteLine("Enter Ticket Count");
             int ticketCount = int.Parse(Console.ReadLine());
 
-            //string insertValue = "Insert Into Train(TrainNumber, TrainName, SourceCity, DestinationCity, TicketPrice, TicketAvailable)"+
-            //    "Values('12022', 'Chennai Express', 'Coimbatore', 'Chennai', 150.34, 120)";
+            string insertValue = "Insert Into Train(TrainNumber, TrainName, SourceCity, DestinationCity, TicketPrice, TicketAvailable)"+
+                "Values('"+trainNumber+ "', ' " +trainName+ "', ' " +sourceCity + "', ' " +destinationCity + "', " +ticketPrice+"," +ticketCount+")";
 
-            string insertValue = "Insert Into Train(TrainNumber, TrainName, SourceCity, DestinationCity, TicketPrice, TicketAvailable)" +
-                "Values(@trainNumber, @trainName, @SourceCity, @DestinationCity, @ticketPrice, @ticketCount)";
-            SqlCommand sqlCommand = new SqlCommand(insertValue, connection);
-
-            sqlCommand.Parameters.AddWithValue("@trainNumber", trainNumber);
-            sqlCommand.Parameters.AddWithValue("@trainName", trainName );
-            sqlCommand.Parameters.AddWithValue("@sourceCity", sourceCity);
-            sqlCommand.Parameters.AddWithValue("@destinationCity", destinationCity);
-            sqlCommand.Parameters.AddWithValue("@ticketPrice", ticketPrice);
-            sqlCommand.Parameters.AddWithValue("@ticketCount", ticketCount);
-            sqlCommand.ExecuteNonQuery();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            dataAdapter.InsertCommand = new SqlCommand(insertValue,connection);
+            dataAdapter.InsertCommand.ExecuteNonQuery();
         }
         public void DisplayTrainInfo(SqlConnection connection)
         {
-            string display = "Select TrainNumber, TrainName, SourceCity, DestinationCity, TicketPrice, TicketAvailable from Train";
-            SqlCommand sqlCommand = new SqlCommand(display, connection);
-            SqlDataReader reader = sqlCommand.ExecuteReader();
-            if (reader.HasRows)
+            string sqlSelect = "select * from Train";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlSelect, connection);
+            DataTable table = new DataTable();
+            dataAdapter.Fill(table);
+            foreach (DataRow row in table.Rows)
             {
-                Console.WriteLine("\nTrainNumber\tTrainName\tSource\tDestination\tTicketPrice\tTicketsAvailable");
-
-                while (reader.Read())
+                foreach (DataColumn column in table.Columns)
                 {
-                    Console.WriteLine(reader.GetString(0) + "\t" + reader.GetString(1)
-                           + "\t" + reader.GetString(2) + "\t" + reader.GetString(3) + "\t" + reader.GetValue(4) + "\t" + reader.GetValue(5));
+                    Console.Write(row[column] + "\t");
                 }
+                Console.WriteLine();
             }
-            else
-            {
-                Console.WriteLine("No rows found.");
-            }
-            reader.Close();
         }
         public void UpdateTrainInfo(string trainNumber,SqlConnection connection)
         {
 
             string updateValue;
             bool status = true;
-            while(status)
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            while (status)
             {
                 Console.WriteLine("Select the field you want to Update?\n1)Train Name\n2)Source City\n3)Destination City\n4)Ticket Price\n5)Ticket Count\n6)Exit");
                 byte choice = Byte.Parse(Console.ReadLine());
@@ -72,26 +60,21 @@ namespace DatabaseConnectivity
                         {
                             Console.WriteLine("Enter the Train Name to Update:");
                             string trainName = Console.ReadLine();
-                            updateValue = "UPDATE Train SET TrainName = @trainName WHERE TrainNumber = @trainNumber";
-                            SqlCommand commandforName = new SqlCommand(updateValue, connection);
-                            commandforName.Parameters.Add("@TrainNumber", System.Data.SqlDbType.VarChar);
-                            commandforName.Parameters.Add("@TrainName", System.Data.SqlDbType.VarChar);
-                            commandforName.Parameters["@TrainNumber"].Value = trainNumber;
-                            commandforName.Parameters["@TrainName"].Value = trainName;
-                            commandforName.ExecuteNonQuery();
+                            updateValue = "UPDATE Train SET TrainName = '"+trainName+"' WHERE TrainNumber = '" + trainNumber + "';";
+                            adapter.UpdateCommand = connection.CreateCommand();
+                            adapter.UpdateCommand.CommandText = updateValue;
+                            adapter.UpdateCommand.ExecuteNonQuery();
+
                         }
                         break;
                     case 2:
                         {
                             Console.WriteLine("Enter the Source City to Update:");
                             string sourceCity = Console.ReadLine();
-                            updateValue = "UPDATE Train SET SourceCity = @sourceCity WHERE TrainNumber = @trainNumber";
-                            SqlCommand commandforsrcCity = new SqlCommand(updateValue, connection);
-                            commandforsrcCity.Parameters.Add("@TrainNumber", System.Data.SqlDbType.VarChar);
-                            commandforsrcCity.Parameters.Add("@SourceCity", System.Data.SqlDbType.VarChar);
-                            commandforsrcCity.Parameters["@TrainNumber"].Value = trainNumber;
-                            commandforsrcCity.Parameters["@SourceCity"].Value = sourceCity;
-                            commandforsrcCity.ExecuteNonQuery();
+                            updateValue = "UPDATE Train SET sourceCity = '" + sourceCity + "' WHERE TrainNumber = '" + trainNumber + "';";
+                            adapter.UpdateCommand = connection.CreateCommand();
+                            adapter.UpdateCommand.CommandText = updateValue;
+                            adapter.UpdateCommand.ExecuteNonQuery();
 
                         }
                         break;
@@ -99,26 +82,22 @@ namespace DatabaseConnectivity
                         {
                             Console.WriteLine("Enter the Destination City to Update:");
                             string destCity = Console.ReadLine();
-                            updateValue = "UPDATE Train SET DestinationCity = @destinationCity WHERE TrainNumber = @trainNumber";
-                            SqlCommand commandforDestCity = new SqlCommand(updateValue, connection);
-                            commandforDestCity.Parameters.Add("@TrainNumber", System.Data.SqlDbType.VarChar);
-                            commandforDestCity.Parameters.Add("@DestinationCity", System.Data.SqlDbType.VarChar);
-                            commandforDestCity.Parameters["@TrainNumber"].Value = trainNumber;
-                            commandforDestCity.Parameters["@DestinationCity"].Value = destCity;
-                            commandforDestCity.ExecuteNonQuery();
+                            updateValue = "UPDATE Train SET DestinationCity = '" + destCity + "' WHERE TrainNumber = '" + trainNumber + "';";
+                            adapter.UpdateCommand = connection.CreateCommand();
+                            adapter.UpdateCommand.CommandText = updateValue;
+                            adapter.UpdateCommand.ExecuteNonQuery();
+
                         }
                         break;
                     case 4:
                         {
                             Console.WriteLine("Enter the Ticket Price to Update:");
                             string ticketPrice = Console.ReadLine();
-                            updateValue = "UPDATE Train SET TicketPrice = @ticketPrice WHERE TrainNumber = @trainNumber";
-                            SqlCommand commandforPrice = new SqlCommand(updateValue, connection);
-                            commandforPrice.Parameters.Add("@TrainNumber", System.Data.SqlDbType.VarChar);
-                            commandforPrice.Parameters.Add("@TicketPrice", System.Data.SqlDbType.Float);
-                            commandforPrice.Parameters["@TrainNumber"].Value = trainNumber;
-                            commandforPrice.Parameters["@TicketPrice"].Value = ticketPrice;
-                            commandforPrice.ExecuteNonQuery();
+                            updateValue = "UPDATE Train SET TicketPrice = " + ticketPrice + " WHERE TrainNumber = '" + trainNumber + "';";
+                            adapter.UpdateCommand = connection.CreateCommand();
+                            adapter.UpdateCommand.CommandText = updateValue;
+                            adapter.UpdateCommand.ExecuteNonQuery();
+
                         }
                         break;
 
@@ -126,14 +105,15 @@ namespace DatabaseConnectivity
                         {
                             Console.WriteLine("Enter the Ticket Count to Update:");
                             string ticketCount = Console.ReadLine();
-                            updateValue = "UPDATE Train SET TicketAvailable = @ticketCount WHERE TrainNumber = @trainNumber";
-                            SqlCommand commandforCount = new SqlCommand(updateValue, connection);
-                            commandforCount.Parameters.Add("@TrainNumber", System.Data.SqlDbType.VarChar);
-                            commandforCount.Parameters.Add("@TicketAvailable", System.Data.SqlDbType.Int);
-                            commandforCount.Parameters["@TrainNumber"].Value = trainNumber;
-                            commandforCount.Parameters["@TicketAvailable"].Value = ticketCount;
-                            commandforCount.ExecuteNonQuery();
+                            updateValue = "UPDATE Train SET TicketAvailable = " + ticketCount + " WHERE TrainNumber = '" + trainNumber + "';";
+                            adapter.UpdateCommand = connection.CreateCommand();
+                            adapter.UpdateCommand.CommandText = updateValue;
+                            adapter.UpdateCommand.ExecuteNonQuery();
+
                         }
+                        break;
+                    case 6:
+                        status = false;
                         break;
                     default:
                         Console.WriteLine("Select a valid choice");
@@ -143,13 +123,14 @@ namespace DatabaseConnectivity
         }
         public void DeleteTrainInfo(SqlConnection connection)
         {
+            SqlDataAdapter adapter = new SqlDataAdapter();
             Console.WriteLine("Enter the Train Number to delete : ");
             string trainNumber = Console.ReadLine();
-            string deleteUser = "DELETE FROM Train where TrainNumber = @trainNumber";
-            SqlCommand command = new SqlCommand(deleteUser, connection);
-            command.Parameters.Add("@TrainNumber", System.Data.SqlDbType.VarChar);
-            command.Parameters["@TrainNumber"].Value = trainNumber;
-            command.ExecuteNonQuery();
+            string deleteUser = "DELETE FROM Train where TrainNumber = '"+trainNumber+" ' ";
+            adapter.DeleteCommand = connection.CreateCommand();
+            adapter.DeleteCommand.CommandText = deleteUser;
+            adapter.DeleteCommand.ExecuteNonQuery();
+
         }
     }
 }
